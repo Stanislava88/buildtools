feee657632a781b36ee361e02e6a71e3fc53db12
slavidichkov <dichkof@gmail.com>
2016-03-01 14:34:25 +0200

    wrote class MyMainProject

---
+++ maven/src/main/java/com/clouway/MyMainProject.java
@@ -0,0 +1,14 @@
+package com.clouway;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Slavi Dichkov (slavidichkof@gmail.com)
+ */
+public class MyMainProject {
+        private static Logger logger = Logger.getLogger(MyMainProject.class);
+
+        public static void main(String[] args) {
+            logger.warn("Main project");
+        }
+}
